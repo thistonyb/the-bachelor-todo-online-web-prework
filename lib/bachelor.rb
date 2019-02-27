02ad1b45d19c35c348a0469e9ae4ef7ffc7d7b68
Tony Baker <this.tonybaker@gmail.com>
2019-02-27 22:07:34 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -7,7 +7,9 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, element|
+    element[:name]
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
