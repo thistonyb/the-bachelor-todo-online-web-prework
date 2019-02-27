3abe828d7d2d06075da6ff290a7ad8e0124ef3e3
Tony Baker <this.tonybaker@gmail.com>
2019-02-27 23:10:09 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -41,7 +41,8 @@ end
 def get_average_age_for_season(data, season)
   average_age = 0
   data[season].each do |details_hash|
-    average_age += details_hash["age"].to_i / data[season].length
+    average_age += details_hash["age"]
   end
+  average_age = average_age / data[season].length
   return average_age
 end
